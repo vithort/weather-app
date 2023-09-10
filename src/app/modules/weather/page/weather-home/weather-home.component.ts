8a3fee6b060ad1cf149ac564995cc82459605164
Vithor Carvalho <vithort@gmail.com>
2023-09-10 19:12:54 +0100

    10. adicionando imagens na aplicação e consumindo serviço de previsão do tempo

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherData } from 'src/app/models/interfaces/weather.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  weatherData!: WeatherData;
 
+  private initialCityName = 'São Paulo';
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherData(this.initialCityName);
+  }
+
+  private getWeatherData(cityName: string): void {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response: WeatherData) => {
+        response && (this.weatherData = response);
+        console.log(this.weatherData);
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
